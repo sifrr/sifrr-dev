967350d96f2862acd8543bc04eadb02f616ec229
Aaditya Taparia <aadityataparia@gmail.com>
2019-05-30 18:07:37 +0900

    chore: add dist files

---
+++ dist/sifrr.dev.js
@@ -0,0 +1,14 @@
+/*! Sifrr.Dev v0.0.1-dev - sifrr project | MIT licensed | https://github.com/sifrr/sifrr-elements */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global = global || self, (global.Sifrr = global.Sifrr || {}, global.Sifrr.Dev = factory()));
+}(this, function () { 'use strict';
+
+	var dev = {};
+
+	return dev;
+
+}));
+/*! (c) @aadityataparia */
+//# sourceMappingURL=sifrr.dev.js.map
---
+++ dist/sifrr.dev.module.js
@@ -0,0 +1,6 @@
+/*! Sifrr.Dev v0.0.1-dev - sifrr project | MIT licensed | https://github.com/sifrr/sifrr-elements */
+var dev = {};
+
+export default dev;
+/*! (c) @aadityataparia */
+//# sourceMappingURL=sifrr.dev.module.js.map
