72cae5ea7cf84a9e4d2051a9f19575fdd3457864
Aaditya Taparia <aadityataparia@gmail.com>
2019-05-31 01:40:21 +0900

    feat: add onDir function to loadDir

--- dist/sifrr.dev.module.js
+++ dist/sifrr.dev.module.js
@@ -84,11 +84,16 @@ var eslintrc = {
   }
 };
 
-function loadDir(dir, onFile, deep = 100) {
+function loadDir({
+  dir,
+  onFile = () => {},
+  onDir = () => {},
+  deep = 100
+} = {}) {
   fs.readdirSync(dir).forEach(file => {
     const filePath = path.join(dir, file);
     fs.statSync(filePath).isDirectory()
-      ? (deep > 0 ? loadDir(filePath, onFile, deep - 1) : () => {})
+      ? (deep > 0 ? (onDir(filePath), loadDir({ dir: filePath, onFile, onDir, deep: deep - 1})) : () => {})
       : onFile(filePath);
   });
 }
